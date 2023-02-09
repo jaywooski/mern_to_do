1e871f1ad35a2b07c79c0d8ef7ad6e5765a7d46a
John Wooley <john.m.wooley@gmail.com>
2023-02-09 12:53:58 -0600

    first commit

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.ATLAS_DB, {
+    useNewUrlParser: true,
+    useunifiedTopology: true
+}
+// , (err) => {
+    // (err) ? console.log(err) : console.log('successfully connected');
+// }
+)
+    .then(() => console.log('Connected to database!'))
+    .catch(console.error);
+
+
+app.listen(3001, ()=> {
+    console.log('hosted on port 3001!!');
+})
